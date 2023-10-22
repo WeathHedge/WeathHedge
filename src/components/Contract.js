dfe42ae8085b8a2bb26327bf899b5960c2d030e9
isha-mistry <ishamistry.lamprostech@gmail.com>
2023-10-22 18:03:04 +0530

    Changed contract address

--- src/components/Contract.js
+++ src/components/Contract.js
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 import weathHedgeABI from "../contracts/artifacts/WeathHedgeABI.json";
 
-export const DERIVATIVE_ADDRESS = "0x10a8C418F73C3fF2d2d2305FDE50A2fD019AF65C";
+export const DERIVATIVE_ADDRESS = "0x4bd75D247926fa13b392EC9C4cB18C7F73d3f193";
 
 export const derivativeInstance = async () => {
   const { ethereum } = window;
