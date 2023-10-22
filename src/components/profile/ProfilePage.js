506e0c6b9c6123e59f1a6e0e6ed6fd6c0f13d57d
isha-mistry <ishamistry.lamprostech@gmail.com>
2023-10-22 20:34:32 +0530

    Made changes in profile page

--- src/components/profile/ProfilePage.js
+++ src/components/profile/ProfilePage.js
@@ -5,6 +5,8 @@ import { derivativeInstance } from "../Contract";
 import { useAccount } from "wagmi";
 import { ClipLoader } from "react-spinners";
 import temp from "../../assets/temp.jpg";
+import { DERIVATIVE_ADDRESS } from "../Contract";
+
 function ProfilePage() {
   const [allUserContracts, setAllUserContracts] = useState([]);
   const { address } = useAccount();
@@ -62,12 +64,35 @@ function ProfilePage() {
     return localDate;
   }
 
+  const transferAmount = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const transactionResponse = await signer.sendTransaction({
+          to: "0x8ca6d6Ef50AD8fcC38358845C76d9606d5d67D07",
+          value: ethers.utils.parseEther("1"),
+        });
+        await transactionResponse.wait();
+        alert(`Transaction successful with hash: ${transactionResponse.hash}`);
+      } else {
+        throw new Error("Ethereum provider not found");
+      }
+    } catch (error) {
+      alert(`Transaction failed with error: ${error.message}`);
+    }
+  };
+
   return (
     <div>
       {/* Withdraw */}
       <div className="d-flex py-4 col-11 mx-auto align-items-center">
         <div className="d-flex col-6 align-items-center">
-          <div className=""> <h2>Withdraw Amount:</h2></div>
+          <div className="">
+            {" "}
+            <h2>Withdraw Amount:</h2>
+          </div>
           <div className="px-3">
             <button className="btn btn-success" onClick={withdrawAmount}>
               {" "}
@@ -76,9 +101,12 @@ function ProfilePage() {
           </div>
         </div>
         <div className="d-flex col-6 align-items-center">
-          <div className=""> <h2>Stake Amount to Vault:</h2></div>
+          <div className="">
+            {" "}
+            <h2>Stake Amount to Vault:</h2>
+          </div>
           <div className="px-3">
-            <button className="btn btn-success" >
+            <button className="btn btn-success" onClick={transferAmount}>
               {" "}
               Stake Here
             </button>
@@ -94,7 +122,10 @@ function ProfilePage() {
 
       {/* Contracts bought */}
       <div>
-        <div className="d-flex col-11 mx-auto py-2"> <h2>Contracts Bought:</h2></div>
+        <div className="d-flex col-11 mx-auto py-2">
+          {" "}
+          <h2>Contracts Bought:</h2>
+        </div>
 
         <div className="row col-11 px-0 user-contracts-main mt-4 py-3 px-sm-3 justify-content-around">
           <div className="d-flex justify-content-center">
@@ -133,16 +164,19 @@ function ProfilePage() {
                     {hexToTimestamp(item.coverageStartDate._hex)}
                   </div>
                   <div className="user-contracts-btn">
-                    Coverage End Date: {hexToTimestamp(item.coverageEndDate._hex)}
+                    Coverage End Date:{" "}
+                    {hexToTimestamp(item.coverageEndDate._hex)}
                   </div>
                   <div className="user-contracts-title">
                     Strike Value: {parseInt(item.strikeValue._hex, 16)} USDC
                   </div>
                   <div className="user-contracts-title">
-                    Premuim Amount: {parseInt(item.premiumAmount._hex, 16) / 1000000} USDC
+                    Premuim Amount:{" "}
+                    {parseInt(item.premiumAmount._hex, 16) / 1000000} USDC
                   </div>
                   <div className="user-contracts-title">
-                    Payout Amount: {parseInt(item.payoutAmount._hex, 16) / 1000000} USDC
+                    Payout Amount:{" "}
+                    {parseInt(item.payoutAmount._hex, 16) / 1000000} USDC
                   </div>
                   <div className="user-contracts-title">
                     Maximum Buyers: {parseInt(item.maxBuyers._hex, 16)}
