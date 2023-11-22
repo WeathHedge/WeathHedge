929069c7a13a0e00153f7229f043b3ef3f90a3d6
Prajapati Yash <77118917+prajapati-yash@users.noreply.github.com>
2023-11-22 11:57:25 +0530

    Updated Hero section of home page

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -4,101 +4,35 @@ import "../../styles/home/Home.css";
 import { Chart } from "chart.js";
 import homeImage from "../../assets/homeImage.png"
 import weathHedgeFlow from "../../assets/WeathHedgeFlow.png"
+import weatherGif from "../../assets/WeatherGif/weather6.gif"
 
 function HomePage() {
-  const chartRef = useRef(null);
 
-  useEffect(() => {
-    const canvas = chartRef.current;
-    const ctx = canvas.getContext("2d");
-
-    const multiply = {
-      beforeDatasetsDraw: (chart, options, el) => {
-        chart.ctx.globalCompositeOperation = "multiply";
-      },
-      afterDatasetsDraw: (chart, options) => {
-        chart.ctx.globalCompositeOperation = "source-over";
-      },
-    };
-
-    const gradientThisWeek = ctx.createLinearGradient(0, 0, 0, 150);
-    gradientThisWeek.addColorStop(0, "#5555FF");
-    gradientThisWeek.addColorStop(1, "#9787FF");
-
-    const gradientPrevWeek = ctx.createLinearGradient(0, 0, 0, 150);
-    gradientPrevWeek.addColorStop(0, "#FF55B8");
-    gradientPrevWeek.addColorStop(1, "#FF8787");
-
-    const config = {
-      type: "line",
-      data: {
-        labels: ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN", "MON"],
-        datasets: [
-          {
-            label: "Temperature",
-            data: [18, 26, 14, 22, 12, 20, 12, 18, 10],
-            fill: false,
-            borderColor: "rgba(255, 255, 255, 0.2)",
-            borderWidth: 2,
-            pointBackgroundColor: "transparent",
-            pointBorderColor: "#FFFFFF",
-            pointBorderWidth: 3,
-            pointHoverBorderColor: "rgba(255, 255, 255, 0.2)",
-            pointHoverBorderWidth: 10,
-            lineTension: 0,
-          },
-        ],
-      },
-      options: {
-        responsive: false,
-        elements: {
-          point: {
-            radius: 6,
-            hitRadius: 6,
-            hoverRadius: 6,
-          },
-        },
-        plugins: {
-          legend: {
-            display: false,
-          },
-          tooltip: {
-            backgroundColor: "transparent",
-            bodyFontSize: 14,
-            callbacks: {
-              label: (context) => {
-                return context.parsed.y + "°C";
-              },
-            },
-          },
-        },
-        scales: {
-          x: {
-            display: false,
-          },
-          y: {
-            display: false,
-            beginAtZero: true,
-          },
-        },
-        plugins: {
-          beforeDraw: (chart) => {
-            chart.clear();
-          },
-          afterDraw: (chart) => {
-            chart.destroy();
-          },
-          plugin: [multiply],
-        },
-      },
-    };
-
-    const myChart = new Chart(ctx, config);
-
-    return () => {
-      myChart.destroy();
-    };
-  }, []);
+ 
+  // const StarryBackground = () => {
+  // useEffect(() => {
+  //   Particles.init({
+  //     selector: '#particles-js',
+  //     color: '#ffffff', // Set color to white (for stars)
+  //     connectParticles: true,
+  //     sizeVariations: 4, // Adjust size variations of stars
+  //     maxParticles: 1000, // Set the maximum number of stars
+  //     responsive: [
+  //       {
+  //         breakpoint: 768,
+  //         options: {
+  //           maxParticles: 200 // Adjust for smaller screens if needed
+  //         }
+  //       },
+  //       {
+  //         breakpoint: 425,
+  //         options: {
+  //           maxParticles: 100 // Adjust for mobile screens if needed
+  //         }
+  //       }
+  //     ]
+  //   });
+  // }, []);}
 
   return (
     <div >
@@ -110,70 +44,43 @@ function HomePage() {
       </div>
       </div> */}
 
-      <div>
-        <div className="bg-dark py-3 my-2">
-          <div className="col-5 ">
-            <h1 className="text-white">Weather-Proof Your Future with <span style={{color:"#93ff00"}}> Weath</span><span style= {{color:"white"}}>Hedge</span>!<br/> Hedge Smart, Hedge Secure.</h1>
-          </div>
-          <div className="col-5">
-              
-          </div>
+      <div >
+      <div className="d-flex" style={{backgroundColor:"black", padding: "4rem 0" }}>
+        <div className="col-5 mx-auto my-auto">
+          <h1 className="text-white">Weather-Proof Your Future with <span style={{ color: "#93ff00" }}>Weath</span><span style={{ color: "white" }}>Hedge</span>!<br /> Hedge Smart, Hedge Secure.</h1>
         </div>
+        <div className="col-5 mx-auto" style={{ width: "500px", height: "500px", borderRadius: "50%", overflow: "hidden" }}>
+          <img src={weatherGif} alt="Weather GIF" style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: "50%" }} />
+        </div>
+      </div>
+
+
+      {/* <div className="  d-flex" style={{backgroundColor:"#ece4db", padding:"6rem  0"}}>
+      <div className="col-5 mx-auto my-auto">
+        <div className=" py-5 px-3" style={{borderRadius:"3rem", backgroundColor:"black"}}>
+        <h1 className="text-white">Weather-Proof Your Future with <span style={{ color: "#93ff00" }}>Weath</span><span style={{ color: "white" }}>Hedge</span>!<br /> Hedge Smart, Hedge Secure.</h1>
+        </div>
+      </div>
+      <div className="col-5 mx-auto">
+        <img src={weatherGif} alt="Weather GIF" style={{ maxWidth: "100%", maxHeight: "100%", borderRadius: "50%" }}/>
+      </div>
+    </div> */}
+
+      {/* <div className="  d-flex" style={{backgroundColor:"black", padding:"6rem  0"}}>
+      <div className="col-5 mx-auto my-auto">
+        <h1 className="text-white">Weather-Proof Your Future with <span style={{ color: "#93ff00" }}>Weath</span><span style={{ color: "white" }}>Hedge</span>!<br /> Hedge Smart, Hedge Secure.</h1>
+      </div>
+      <div className="col-5 mx-auto">
+        <img src={weatherGif} alt="Weather GIF" width={500} style={{ maxWidth: "100%", maxHeight: "100%", borderRadius: "50%" }}/>
+      </div>
+    </div> */}
       </div>
 
       
 
       <div className=" home-title my-auto" style={{paddingTop:"5rem", paddingBottom:"2.5rem"}}> <span style={{fontSize:"2rem", fontWeight:"700", backgroundColor:"black", width:"70%", borderRadius:"2.5rem",padding:"0.8rem 5rem "}}><span className="text-white">About  </span> <span style={{color:"#93ff00"}}> Weath</span><span style={{color:"white"}}>Hedge</span></span></div>
       <div className="d-flex mx-auto">
-      <div>
-        <div className="col-5 mx-auto py-5" style={{paddingLeft:"8rem"}}>
-          <div className="card">
-          <div className="about">
-            <p className="lead">Temperature in °C</p>
-          </div>
-
-          <canvas ref={chartRef} id="canvas"></canvas>
-
-          <div className="axis">
-            <div className="tick">
-              <span className="day-number">10</span>
-              <span className="day-name">MON</span>
-              <span className="value value--this">26°C</span>
-            </div>
-            <div className="tick">
-              <span className="day-number">11</span>
-              <span className="day-name">TUE</span>
-              <span className="value value--this">14°C</span>
-            </div>
-            <div className="tick">
-              <span className="day-number">12</span>
-              <span className="day-name">WED</span>
-              <span className="value value--this">22°C</span>
-            </div>
-            <div className="tick">
-              <span className="day-number">13</span>
-              <span className="day-name">THU</span>
-              <span className="value value--this">12°C</span>
-            </div>
-            <div className="tick">
-              <span className="day-number">14</span>
-              <span className="day-name">FRI</span>
-              <span className="value value--this">20°C</span>
-            </div>
-            <div className="tick">
-              <span className="day-number">15</span>
-              <span className="day-name">SAT</span>
-              <span className="value value--this">12°C</span>
-            </div>
-            <div className="tick">
-              <span className="day-number">16</span>
-              <span className="day-name">SUN</span>
-              <span className="value value--this">18°C</span>
-            </div>
-          </div>
-          </div>
-        </div>
-      </div>
+    
       <div className="home-content-component col-7 mx-auto" >
         
         <div className="col-10 mx-auto text-left" style={{textAlign:"left", fontSize:"1.3rem", color:"#93ff00", backgroundColor:"black", padding:"2rem", borderRadius:"4rem"}}>
